34039503e80bcf896053765e11b5f76ce82013e5
Presunto Parma <arthurgobbetti@gmail.com>
2026-05-28 22:52:18 -0300

    Implemented camera on all rooms

--- Assets/Project/Dev/Scripts/Lever/LeverNextStep.cs
+++ Assets/Project/Dev/Scripts/Lever/LeverNextStep.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class LeverNextStep : LeverBase
 {
     [SerializeField] private GameManager gameManager;
+    private bool disabled = false;
 
 
     protected override void Activate(bool i)
     {
         base.Activate(i);
 
-        if (i)
+        if (i && !disabled)
+        {
             gameManager.NextLevel();
+            disabled = true;
+        }
     }
 }
